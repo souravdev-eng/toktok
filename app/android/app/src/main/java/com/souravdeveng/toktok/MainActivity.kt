d67feb6731b45bd36b4aabfa4cdd96fb38f5b9ac
souravdev <souravmajumdar.developer@gmail.com>
2024-01-01 10:56:05 +0530

    Rename project and logo

--- app/android/app/src/main/java/com/souravdeveng/toktok/MainActivity.kt
+++ app/android/app/src/main/java/com/souravdeveng/toktok/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app
+package com.souravdeveng.toktok
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- app/android/app/src/main/java/com/souravdeveng/toktok/MainApplication.kt
+++ app/android/app/src/main/java/com/souravdeveng/toktok/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.app
+package com.souravdeveng.toktok
 
 import android.app.Application
 import com.facebook.react.PackageList
